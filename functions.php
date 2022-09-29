ac018d64042deaee7b630c02d5a32cf0245d78d1
Jon Rickard Yxelflod <rickard.yxelflod@dragster.se>
2022-09-29 20:59:56 +0200

    testing

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ add_action('wp_enqueue_scripts', 'yxelflod_files');
 
 function yxelflod_files()
 {
-  wp_enqueue_style('main_style', get_style_sheet_uri('/assets/css/countdown-timer.css'));
+  wp_enqueue_style('main_style', get_style_sheet_uri('assets/css/countdown-timer.css'));
   // wp_enqueue_script('countdown_timer', get_theme_file_uri('/assets/js/countdown-timer.js'), false, '1.0', true);
 }
 
