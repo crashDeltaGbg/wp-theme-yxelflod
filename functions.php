2ff65d283bb43d21da1bbc9bb52bc2d2d0ea1dd1
Jon Rickard Yxelflod <rickard.yxelflod@dragster.se>
2022-09-29 21:27:33 +0200

    testing

--- functions.php
+++ functions.php
@@ -6,9 +6,9 @@ function yxelflod_files()
 {
   wp_enqueue_style('main_style', get_stylesheet_uri('style.css'));
   wp_enqueue_style('countdown_timer_style', get_theme_file_uri('/assets/css/countdown-timer.css'), false, '1.0', 'all');
-  // wp_enqueue_script('countdown_timer_script', get_theme_file_uri('/assets/js/countdown-timer.js'), false, '1.0', true);
+  wp_enqueue_script('countdown_timer_script', get_theme_file_uri('/assets/js/countdown-timer.js'), false, '1.0', true);
 }
 
-/* if (function_exists('acf_add_options_page')) {
+if (function_exists('acf_add_options_page')) {
   acf_add_options_page();
-} */
+}
--- page-heads-up.php
+++ page-heads-up.php
@@ -6,9 +6,9 @@ $date_time = get_field('next_event_date_time');
 
 ?>
 
-<h1>Page heads up</h1>
-<!-- <div data-event="<?php // echo $date_time;  ?>" id="countdown-timer"></div> -->
-<?php print_r($date_time); ?>
+<!-- <h1>Page heads up</h1> -->
+<div data-event="<?php echo $date_time;  ?>" id="countdown-timer"></div>
+<?php // print_r($date_time); ?>
 
 <?php
 
