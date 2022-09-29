0e7988836960471342a3757321e852ced68f14d7
Jon Rickard Yxelflod <rickard.yxelflod@dragster.se>
2022-09-29 21:53:58 +0200

    testing

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer>
-  <p>&copy; -:[CrashDelta]:- 2022 &mdash; <?php echo date('Y'); ?></p>
+  <p id="copyright">&copy; -:[CrashDelta]:- 2022 &mdash; <?php echo date('Y'); ?></p>
 </footer>
 
 <?php wp_footer(); ?>
