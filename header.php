9d144f725bb3f6547e3b3e591012706f8ac9b927
Jon Rickard Yxelflod <rickard.yxelflod@dragster.se>
2022-09-29 21:00:55 +0200

    testing

--- header.php
+++ header.php
@@ -7,8 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php // the_title(); 
           ?></title>
-  <?php // wp_head(); 
-  ?>
+  <?php wp_head(); ?>
 </head>
 
 <body>
