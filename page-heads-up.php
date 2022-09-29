0bd439de5f4d62e4875a0e73456215b6272faf98
Jon Rickard Yxelflod <rickard.yxelflod@dragster.se>
2022-09-29 20:53:01 +0200

    testing

--- page-heads-up.php
+++ page-heads-up.php
@@ -2,14 +2,13 @@
 
 // get_header();
 
-// $date_time = get_field('next_event_date_time');
-
-// print_r($date_time);
+$date_time = get_field('next_event_date_time');
 
 ?>
 
 <h1>Page heads up</h1>
 <!-- <div data-event="<?php // echo $date_time;  ?>" id="countdown-timer"></div> -->
+<?php print_r($date_time); ?>
 
 <?php
 
