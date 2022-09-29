89b8b0253ca8172da3851baad5be78b3479e28a5
Jon Rickard Yxelflod <rickard.yxelflod@dragster.se>
2022-09-29 20:10:45 +0200

    format header.php

--- header.php
+++ header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <?php wp_head(); ?>
   <meta charset="UTF-8">
@@ -7,4 +8,5 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php the_title(); ?></title>
 </head>
+
 <body>
