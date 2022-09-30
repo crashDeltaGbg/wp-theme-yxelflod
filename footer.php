ecbaa9cc1ef6a3555b59a72b717f4d4e5b9db261
Jon Rickard Yxelflod <rickard.yxelflod@dragster.se>
2022-09-30 17:17:50 +0200

    change copyright logo

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer>
-  <p id="copyright">&copy; 2022 &mdash; <?php echo date('Y'); ?> -:[CrashDelta]:-</p>
+  <p id="copyright">&copy; 2022 &mdash; <?php echo date('Y'); ?> -:[jRy]:-</p>
 </footer>
 
 <?php wp_footer(); ?>
