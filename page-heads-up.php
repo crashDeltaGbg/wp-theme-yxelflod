81428c8f62bcebe8e6639ae309ddf402d4882415
Jon Rickard Yxelflod <rickard.yxelflod@dragster.se>
2022-10-01 16:47:07 +0200

    change fa class

--- page-heads-up.php
+++ page-heads-up.php
@@ -10,9 +10,11 @@ $canceled = get_field('next_event_cancel');
 <header>
   <div class="social-links">
     <a href="" id="twitter" title="Heads-up Events on Twitter" target="_blank">
-      <i class="fa-regular">&#xf099;</i></a>
+      <i class="fa-brands">&#xf099;</i>
+    </a>
     <a href="" id="instagram" title="Heads-up Events on Instagram" target="_blank">
-      <i class="fa-regular">&#xf16d;</i></a>
+      <i class="fa-brands">&#xf16d;</i>
+    </a>
   </div>
 </header>
 
