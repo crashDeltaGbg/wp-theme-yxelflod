13dd5e36aef67471d1ec0d937dc6ccee6d5fba11
Jon Rickard Yxelflod <rickard.yxelflod@dragster.se>
2022-10-01 10:53:01 +0200

    add canceled message to heads-up page

--- page-heads-up.php
+++ page-heads-up.php
@@ -3,14 +3,21 @@
 get_header();
 
 $date_time = get_field('next_event_date_time');
+$canceled = get_field('next_event_cancel');
+
+if (!$canceled) : 
 
 ?>
 
-<!-- <h1>Page heads up</h1> -->
 <div data-event="<?php echo $date_time;  ?>" id="countdown-timer"></div>
-<?php // print_r($date_time); ?>
+
+<?php else : ?>
+
+  <div id="canceled">canceled</div>
 
 <?php
+
+endif;
 
 get_footer();
 
