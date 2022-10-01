887000ce7b4b8f02f8dd1a1de8a07a9cc30c4826
Jon Rickard Yxelflod <rickard.yxelflod@dragster.se>
2022-10-01 18:11:46 +0200

    add favicon

--- header.php
+++ header.php
@@ -9,7 +9,11 @@
     <meta name="twitter:title" content="Next Event" />
     <meta name="twitter: site" content="@heads_up_events" />
     <meta name="twitter: creator" content="@heads_up_events" />
-    <meta name="twitter: image" content="<?php the_post_thumbnail_url('large'); ?>">
+    <meta name="twitter: image" content="<?php the_post_thumbnail_url('large'); ?>" />
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php site_url('/assets/img/favicon_io/apple-touch-icon.png') ?>">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php site_url('/assets/img/favicon_io/favicon-32x32.png') ?>">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php site_url('/assets/img/favicon_io/favicon-16x16.png') ?>">
+    <link rel="manifest" href="<?php site_url('site.webmanifest') ?>">
   <?php endif; ?>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
