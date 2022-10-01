92d46d4716b7b32733c923c858fe843ff3170122
Jon Rickard Yxelflod <rickard.yxelflod@dragster.se>
2022-10-01 16:40:39 +0200

    add header to page-heads-up.php

--- page-heads-up.php
+++ page-heads-up.php
@@ -5,18 +5,22 @@ get_header();
 $date_time = get_field('next_event_date_time');
 $canceled = get_field('next_event_cancel');
 
-if (!$canceled) : 
-
 ?>
 
 <header>
   <div class="social-links">
     <a href="" id="twitter" title="Heads-up Events on Twitter" target="_blank">&#xf099;</a>
-    <a href="" id="instagram" title="Heads-up Events on Instagram"target="_blank">&#xf16d;</a>
+    <a href="" id="instagram" title="Heads-up Events on Instagram" target="_blank">&#xf16d;</a>
   </div>
 </header>
 
-<div data-event="<?php echo $date_time;  ?>" id="countdown-timer"></div>
+<?php
+
+if (!$canceled) :
+
+?>
+
+  <div data-event="<?php echo $date_time;  ?>" id="countdown-timer"></div>
 
 <?php else : ?>
 
