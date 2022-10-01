c7bc25722f09ae53c81e8fab44c3297446896d62
Jon Rickard Yxelflod <rickard.yxelflod@dragster.se>
2022-10-01 13:25:55 +0200

    add code for twitter meta content in head if page is heads-up

--- header.php
+++ header.php
@@ -2,7 +2,14 @@
 <html lang="en">
 
 <head>
-  <?php the_field('html_head_meta_content'); ?>
+  <?php
+  if (get_field('html_head_meta_content')) the_field('html_head_meta_content');
+  if (is_page('heads-up')) : ?>
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter: site" content="@heads_up_events" />
+    <meta name="twitter: creator" content="@heads_up_events" />
+    <meta name="twitter: image" content="<?php the_post_thumbnail_url('large'); ?>">
+  <?php endif; ?>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
