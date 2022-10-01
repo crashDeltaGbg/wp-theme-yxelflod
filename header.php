a008121223ea177e5bf28e0c642cba95ccb8b2ae
Jon Rickard Yxelflod <rickard.yxelflod@dragster.se>
2022-10-01 13:18:15 +0200

    add code for injecting meta content in head

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html lang="en">
 
 <head>
+  <?php the_field('html_head_meta_content'); ?>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
